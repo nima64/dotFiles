0abf6f45e6e344ebf1e13be2cfb4313a0c21e61d
nima64 <kennethe407@gmail.com>
2022-09-03 11:13:05 -0700

    initial commit

---
+++ freshInstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+APPS="gcc g++ gdb git clangd evince"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+set -x
+apt update
+apt install -y $APPS
+
+# Install vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
